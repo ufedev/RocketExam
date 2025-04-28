50d364e16ba4c236e623e1e5e652f27c9a999d24
Federico Malfasi <federico.malfasi@gmail.com>
2025-04-28 18:47:02 -0300

    Finalizado para Javascript

--- back/config/db.ts
+++ back/config/db.ts
@@ -1,8 +1,13 @@
 import { Sequelize } from 'sequelize'
 
 export const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: `./${process.env.DB_NAME}`
+  dialect: 'mysql',
+  host: '0.0.0.0',
+  port: 3306,
+  username: 'root',
+  password: 'aezakmi',
+  database: 'rocketexam'
+  // storage: `./${process.env.DB_NAME}`
 })
 
 console.log(process.env.DB_NAME)
