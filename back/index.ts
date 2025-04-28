766de247a68fe426d5a0d99b1e0febd051a3bccb
Federico Malfasi <federico.malfasi@gmail.com>
2025-04-28 17:47:46 -0300

    Finalizado para Javascript

--- back/index.ts
+++ back/index.ts
@@ -13,6 +13,9 @@ import { examRouter } from './routes/exam'
 import { examResRouter } from './routes/exam_results'
 import cors from 'cors'
 // APP
+
+const port = process.argv[2] ?? 3000
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -30,11 +33,12 @@ app.use('/api', examRouter)
 // Resutls
 app.use('/api', examResRouter)
 
-app.listen(3000, async () => {
+app.listen(port, async () => {
   try {
     await sequelize.authenticate()
     await sequelize.sync()
     console.log('Base de datos conectada')
+    console.log('Servidor corriendo en puerto ' + port)
   } catch {
     console.error('No se pudo conectar a la  base de datos')
   }
